e7092c6f8ba3433bfa20f19a7e0082706720296c
Felix Lie <felixlie19@gmail.com>
2021-07-30 17:42:02 -0700

    query most registration by day of week

--- query.sql
+++ query.sql
@@ -4,3 +4,11 @@
 SELECT * FROM users
 ORDER BY created_at
 LIMIT 5;
+
+-- 2. What day of the week do most users register on?
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC;
