8eed63083e1a383f32ca2cad4a339077728a6a22
Felix Lie <felixlie19@gmail.com>
2021-07-30 17:06:27 -0700

    created user's table

--- createTable.sql
+++ createTable.sql
@@ -1,3 +1,10 @@
 ---------------Create new database in GoormIDE---------------
 CREATE DATABASE ig_clone;
 USE ig_clone; 
+
+---------------Create New Tables---------------
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
